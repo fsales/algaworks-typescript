31116292610b8a1b69bf1c531b1d81f59a6f91a9
Fábio Sales <fabio.oliveira.sales@gmail.com>
2020-05-15 22:09:26 -0300

    fundamentos typesscript

---
+++ fundamentos/es6/arrow-functions.js
@@ -0,0 +1,13 @@
+const precos = [10, 23, 19, 40, 50];
+// sem arrow functions
+const valorEmReias = precos.map(function (preco) {
+    return `R$ ${preco} ,00`;
+});
+console.log(valorEmReias);
+// com arrow functions
+const precoEmReaisA = precos.map(preco => { return `R$ ${preco} ,00`; });
+console.log(precoEmReaisA);
+const precoEmReaisA2 = precos.map(preco => `R$ ${preco} ,00`);
+console.log(precoEmReaisA2);
+const acimaDeVinte = precos.filter(preco => preco > 20).map(preco => `R$ ${preco} ,00`);
+console.log(acimaDeVinte);
